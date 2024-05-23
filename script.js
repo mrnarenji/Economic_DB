913e17a81369aa0e281ecc956b7df6f84e5f59ce
mrnarenji <95470357+mrnarenji@users.noreply.github.com>
2024-05-23 14:30:26 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-Papa.parse("PUBLISHED_CSV_LINK_HERE", {
+Papa.parse("https://docs.google.com/spreadsheets/d/e/2PACX-1vQeLneeRd3x3cHvNl8WRzl9wZOPg9PJwrqpNCAPaIzqAfN2Ri9SMDTclqlE1lccJlte60SuBOJqKWeW/pub?gid=714893787&single=true&output=csv", {
     download: true,
     complete: function(results) {
         let data = results.data;
